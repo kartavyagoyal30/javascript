3eef0fbcbe89f544a59a46b9b07393e91326ce93
goyal-demo <kartavyagoyal1234@gmail.com>
2026-05-31 23:42:40 +0530

    to string

---
+++ Lect-3.js
@@ -0,0 +1,46 @@
+//Number
+// let a=10;
+// let b=123.234;
+// console.log(b.toFixed(2));
+// console.log(typeof b.toFixed(2));
+// console.log(b);
+// console.log(b.toPrecision(4));
+// console.log(b.toString());
+
+// let a=new Number(20);
+// let b=new Number(20);
+// console.log(a);
+// console.log(typeof a);
+// console.log(a==b);
+
+// let obj1={
+//     name:"Rohit"
+// }
+
+// let obj2={
+//     name:"Rohit"
+// }
+
+// let obj2=obj1;
+
+// console.log(obj1==obj2);
+
+//Non Primitive:Reference basis pe compare honge
+//Primitive: Copy of value
+
+// console.log(Math.abs(-10));
+// console.log(Math.LN10);
+// console.log(Math.PI);
+
+// console.log(Math.random());
+
+// console.log(Math.floor(Math.random()*10));  ->(0-9)
+
+// console.log(Math.floor(Math.random()*10)+1);  //1-10
+
+// console.log(Math.floor(Math.random()*6)+1); //1-6
+
+// console.log(Math.floor(Math.random()*11)+15); //15-25
+
+console.log(Math.floor(Math.random()*(9999-1000+1))+1000);
+ 
---
+++ Lect-4.js
@@ -0,0 +1,42 @@
+const str = "Kartavya";
+const str1 = "Kartavya Goyal";
+const str2 = `Kartavya 
+Goyal`;
+
+// console.log(str);
+// console.log(str1);
+// console.log(str2);
+
+// console.log(str.length);
+// console.log(str[1]);
+
+// str[2]="s";
+// console.log(str);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// console.log(str.indexOf('a'));
+// console.log(str.lastIndexOf('a'));
+
+// console.log(str.includes('a'));
+
+// console.log(str.slice(2, 5));
+// console.log(str.slice(-2));
+
+// console.log(str.substring(2,5));
+
+// const a = "kartavya"
+// const b = "goyal"
+// const c=a+b;
+// // console.log(c);
+
+// console.log(24+"xyxx");
+// console.log(24+ 'xyxx'+10);
+// console.log(24+10+'xyxx');
+
+// console.log(str.replace('a', 's'));
+
+const names="Rohit,Mohit,Suraj,Rohan,Anjali";
+
+console.log(names.split(','));
